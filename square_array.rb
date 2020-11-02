2be1fc35dd38dfa99e155db794e86afa72adda35
Hannah Malm <hannahemalm616@gmail.com>
2020-11-02 00:15:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,6 @@
-# def square_array(array)
-#   array = [1,2,3]
-#   array.each {|i| array << i ** 2 }
-#   array
-# end
 
 def square_array(array)
   arr = []
   array.each { |i| arr << i ** 2 }
   arr
 end
-
-# my_arr = [1, 2]
-# p square_array(my_arr)
